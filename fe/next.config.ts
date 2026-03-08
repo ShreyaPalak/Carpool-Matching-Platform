402b2b3c6660d14c6899df8c84838a9b2bcb6c23
ShreyaPalak <168367329+ShreyaPalak@users.noreply.github.com>
2026-03-08 15:17:04 +0000

    Request form and ui redesigning

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "imgd.aeplcdn.com" },
+      { protocol: "https", hostname: "www.financialexpress.com" },
+      { protocol: "https", hostname: "europeanlifemedia.com" },
+      { protocol: "https", hostname: "hips.hearstapps.com" },
+      { protocol: "https", hostname: "di-uploads-pod9.dealerinspire.com" },
+    ],
+  },
 };
 
 export default nextConfig;
